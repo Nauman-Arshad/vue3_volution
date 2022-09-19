70bdb4d95c6dbddda749e97437834e1f1b3d1242
Nauman-Arshad <naumankhan642@gmail.com>
2022-09-19 23:18:08 +0500

    v-show derictive

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
   <h3 v-bind:class="status">Status</h3>
   <h2 v-bind:class="isPromoted && 'promoted'">Promoted Movie</h2>
   <h2 v-bind:class="isSoldout ? 'sold_out' : 'new'">Sold out movie</h2> -->
-  <h2 v-if="num === 0">The number is zero</h2>
+  <!-- <h2 v-if="num === 0">The number is zero</h2>
   <h2 v-else-if="num < 0">The number is Negative</h2>
   <h2 v-else-if="num > 0">The number is Positive</h2>
   <h2 v-else>Not a Number</h2>
@@ -15,7 +15,14 @@
   <h2>Hello</h2>
   <h2>Nauman</h2>
   <h2>Vue</h2>
-  </div>
+  </div> -->
+  <template>
+  <h2 v-if="num === 0">Number is 0</h2>
+  <h2 v-else-if="num > 0">Number is +</h2>
+  <h2 v-else-if="num < 0">Number is -</h2>
+  <h2 v-else>Not a Number</h2>
+  </template>
+  <h2 v-show="showElement">using v-show</h2>
 </div>
 </template>
 
@@ -29,8 +36,9 @@ export default {
       // headingId: 'heading',
       // isPromoted: true,
       // isSoldout: true
-      num: 12,
-      display: true
+      // num: 12,
+      // display: true
+      showElement: true
     };
   }
 }

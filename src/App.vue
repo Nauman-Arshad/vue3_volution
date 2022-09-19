9e5955ff082b07604723fe9ceb43dcca96ed4261
Nauman-Arshad <naumankhan642@gmail.com>
2022-09-19 23:04:59 +0500

    Added conditional rendering in vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
 <div>
-  <h5>Hello {{name}}</h5>
+  <!-- <h5>Hello {{name}}</h5>
   <div v-html="channel"></div>
-  <h2 :id="headingId">Heading</h2>
+  <h2 v-bind:id="headingId">Heading</h2>
   <h2 class="underline">Underline text</h2>
-  <h3 :class="status">Status</h3>
-  <h2 :class="isPromoted && 'promoted'">Promoted Movie</h2>
-  <h2 :class="isSoldout ? 'sold_out' : 'new'">Sold out movie</h2>
+  <h3 v-bind:class="status">Status</h3>
+  <h2 v-bind:class="isPromoted && 'promoted'">Promoted Movie</h2>
+  <h2 v-bind:class="isSoldout ? 'sold_out' : 'new'">Sold out movie</h2> -->
+  <h2 v-if="num === 0">The number is zero</h2>
+  <h2 v-else-if="num < 0">The number is Negative</h2>
+  <h2 v-else-if="num > 0">The number is Positive</h2>
+  <h2 v-else>Not a Number</h2>
+<div v-if="display">
+  <h2>Hello</h2>
+  <h2>Nauman</h2>
+  <h2>Vue</h2>
+  </div>
 </div>
 </template>
 
@@ -15,11 +24,13 @@ export default {
   name: 'App',
   data() {
     return {
-      name: 'Nauman',
-      channel: '<b>codecode</b>',
-      headingId: 'heading',
-      isPromoted: true,
-      isSoldout: true
+      // name: 'Nauman',
+      // channel: '<b>codecode</b>',
+      // headingId: 'heading',
+      // isPromoted: true,
+      // isSoldout: true
+      num: 12,
+      display: true
     };
   }
 }

6a890d3faa683f0c154aaba25a4ba55d9afe7507
Nauman-Arshad <naumankhan642@gmail.com>
2022-09-19 22:18:57 +0500

    Initails commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+  <h5>Hello {{name}}</h5>
+  <div v-html="channel"></div>
+  <h2 v-bind:id="headingId">Heading</h2>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      name: 'Nauman',
+      channel: '<b>codecode</b>',
+      headingId: 'heading'
+    };
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>

a9c3fb6388de6319772ffc42e85b9a01b379a539
Nauman-Arshad <naumankhan642@gmail.com>
2022-09-19 22:52:24 +0500

    read bind in vue js

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,11 @@
 <div>
   <h5>Hello {{name}}</h5>
   <div v-html="channel"></div>
-  <h2 v-bind:id="headingId">Heading</h2>
+  <h2 :id="headingId">Heading</h2>
+  <h2 class="underline">Underline text</h2>
+  <h3 :class="status">Status</h3>
+  <h2 :class="isPromoted && 'promoted'">Promoted Movie</h2>
+  <h2 :class="isSoldout ? 'sold_out' : 'new'">Sold out movie</h2>
 </div>
 </template>
 
@@ -13,12 +17,26 @@ export default {
     return {
       name: 'Nauman',
       channel: '<b>codecode</b>',
-      headingId: 'heading'
+      headingId: 'heading',
+      isPromoted: true,
+      isSoldout: true
     };
   }
 }
 </script>
 
 <style>
+.underline{
+  text-decoration: underline;
+}
+.promoted{
+  font-style: italic;
+}
 
+.new{
+  color: green;
+}
+.sold_out{
+  color: orange
+}
 </style>
